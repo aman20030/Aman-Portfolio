315d9201c1822dede7dba03e3935fc7909156f49
aman20030 <2022pcecaaman004@poornima.org>
2025-04-07 14:29:40 +0530

    change of photo

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -5,7 +5,7 @@ export const portfolioData = {
       "name": "Aman Singhal",
       "headline": "Aspiring Software Developer | Web Development Enthusiast | Passionate About Open-Source & AI | Data Enthusiast | Open Source Contributor",
       "summary": "I am a passionate Computer Science and Engineering (AI) student with a keen interest in web development, artificial intelligence, and open-source contributions. With hands-on experience in React, Tailwind CSS, TypeScript, and database management, I enjoy building modern, user-friendly web applications that solve real-world problems. Beyond coding, I actively contribute to open-source projects, mentor peers, and participate in technical initiatives that foster learning and collaboration. My goal is to continuously enhance my technical skills, work on impactful projects, and contribute meaningfully to the tech community.Always eager to learn and explore new technologies, I strive to bridge the gap between academic knowledge and real-world applications, preparing myself for a successful career in technology. 🚀",
-      "image": "https://github.com/aayushsh2003/AIClassOf26/blob/main/rbi_aayush.png?raw=true"
+      "image": "/images/aman.png"
     },
     "location": {
       "city": "Jaipur",
@@ -13,19 +13,19 @@ export const portfolioData = {
       "country": "India"
     },
     "contactInfo": {
-      "email": "aayushsharma4437@gmail.com",
-      "phone": "+91-1",
-      "website": "https://aayush-ki-pehchan.vercel.app/"
+      "email": "amansinghal2027@gmail.com",
+      "phone": "+91-",
+      "website": ""
     },
     "profiles": {
-      "linkedin": "https://www.linkedin.com/in/aayush-sharma-a44062299/",
-      "instagram": "https://www.instagram.com/aayushsh2003",
-      "twitter": "https://x.com/aayushSh2003",
-      "github": "https://github.com/aayushsh2003",
-      "hackerrank": "https://www.hackerrank.com/profile/aayushsharma4437",
+      "linkedin": "",
+      "instagram": "",
+      "twitter": "",
+      "github": "https://github.com/aman20030",
+      "hackerrank": "",
       "hackerearth": "",
       "codechef": "",
-      "leetcode": "https://leetcode.com/u/aayushsh2003/"
+      "leetcode": ""
     }
   },
   "about": {
@@ -145,109 +145,9 @@ export const portfolioData = {
         "Open Source Contributor"
       ]
         
-    },
-    {
-        "degree": "Senior Secondary (Grade 12)",
-        "fieldOfStudy": "PCM with Computer Science and Physical Education",
-        "institution": {
-            "name": "Kendriya Vidyalaya No. 5 Jaipur",
-            "url":""
-        },
-        "location": {
-            "city": "Jaipur",
-            "state": "Rajasthan",
-            "country": "India"
-        },
-        "duration": {
-            "start": "2020-06",
-            "end": "2022-07"
-        },
-        "score": "Percentage: 91.6%",
-        "relevantCoursework": [
-            "Physics", "Chemistry", "Mathematics", "Computer Science", "Physical Education","English"
-      ],
-      "honors": [
-        "Certificate of Merit in Mathematics Standard",
-        "Top 0.1% in Mathematics Standard (CBSE)"
-      ],
-      "extracurricularActivities": [
-        "Member of Raman House",
-        "Project Head in Programming Project",
-        "Class Monitor"
-      ]
-    },
-    {
-        "degree": "Secondary (Grade 10)",
-        "fieldOfStudy": "General Studies",
-        "institution": {
-            "name": "Kendriya Vidyalaya No. 5 Jaipur",
-            "url":""
-        },
-        "location": {
-            "city": "Jaipur",
-            "state": "Rajasthan",
-            "country": "India"
-        },
-        "duration": {
-            "start": "2010-06",
-            "end": "2020-06"
-        },
-        "score": "Percentage: 89.6%",
-        "relevantCoursework": [
-            "Mathematics", "Science", "Social Science", "English", "Hindi"
-      ],
-      "honors": [
-        "Certificate of Merit in Mathematics Standard",
-        "Top 0.1% in Mathematics Standard (CBSE)"
-      ],
-      "extracurricularActivities": [
-        "Technical Lead, Programming Club",
-        "Organizer, Annual Tech Fest",
-        "Open Source Contributor"
-      ]
-    },
-    {
-        "degree": "RS-CIT (Rajasthan State Certificate in Information Technology)",
-        "fieldOfStudy": "Information Technology",
-        "institution": {
-            "name": "Vardhman Mahaveer Open University (VMOU), Kota",
-            "url":"https://www.vmou.ac.in/"
-        },
-        "location": {
-            "city": "Kota",
-            "state": "Rajasthan",
-            "country": "India"
-        },
-        "duration": {
-            "start": "2018-01",
-            "end": "2018-10"
-        },
-        "score": "Percentage: 85%",
-        "relevantCoursework": [
-            "Computer Fundamentals", "Internet", "MS Office", "E-Governance"  
-      ],
-        "honors": [
-      ],
-      "extracurricularActivities": [
-      ]
     }
   ],
   "projects": [
-    {
-      "title": "Handwriting Converter - Transform Digital Text to Handwritten Notes",
-      "description": "A web-based application that converts digital text into authentic handwritten notes, providing a personal touch for students, educators, and professionals.",
-      "objectives": [
-        "To automate the conversion of digital text into realistic handwriting.",
-        "To offer customization options for various handwriting styles."
-      ],
-      "outcomes": [
-        "Developed a user-friendly interface with real-time text transformation.",
-        "Enhanced accessibility and usability for students and professionals."
-      ],
-      "url": "https://handwriting-converter.vercel.app/",
-      "technologiesUsed": ["React.js", "Tailwind CSS", "Vite", "shadcn-ui"],
-      "image": "https://github.com/aayushsh2003/tailwindportfolio/blob/main/ALL_Things/Projects/Hand_writing_converter.jpeg?raw=true"
-    },
     {
       "title": "Aman Singhal Linktree",
       "description": "A personal Linktree webpage that consolidates my web development projects, resume, and contributions. It provides direct access to my React, Next.js, and MERN stack portfolios, as well as key projects such as a Weather App and a Financial Smart Accounting replica.",
@@ -470,83 +370,6 @@ export const portfolioData = {
         "description": "Badge awarded for AI awareness.",
         "issueDate": "2024-12",
         "image": "https://github.com/aayushsh2003/tailwindportfolio/blob/main/ALL_Things/Certificates/Aayush%20Sharma_AI_AWARE_CERTIFICATE.png?raw=true"
-      },
-      {
-        "title": "Educational Leadership",
-        "issuingOrganization": {
-          "name": "Indian Institute of Technology, Kharagpur",
-          "url": "https://nptel.ac.in/"
-        },
-        "description": "NPTEL course on Educational Leadership with a score of 83%.",
-        "issueDate": "2024-11",
-        "credentialID": "NPTEL24HS159S1051800977",
-        "image": "https://github.com/aayushsh2003/tailwindportfolio/blob/main/ALL_Things/NPTEL/Educational%20Leadership.png?raw=true"
-      },
-      {
-        "title": "Public Speaking",
-        "issuingOrganization": {
-          "name": "Indian Institute of Technology, Roorkee",
-          "url": "https://nptel.ac.in/"
-        },
-        "description": "Completed NPTEL course on Public Speaking with a score of 61%.",
-        "issueDate": "2024-11",
-        "credentialID": "NPTEL24HS143S851801191",
-        "image": "https://github.com/aayushsh2003/tailwindportfolio/blob/main/ALL_Things/NPTEL/Public%20Speaking.png?raw=true"
-      },
-      {
-        "title": "Exploration of the Solar System",
-        "issuingOrganization": {
-          "name": "Indian Institute of Remote Sensing (IIRS), ISRO",
-          "url": "https://www.iirs.gov.in/"
-        },
-        "description": "Studied planetary science, lunar exploration missions, and space research.",
-        "issueDate": "2024-06",
-        "credentialID": "4134a82ca7b0e643acdf5bd8ad9f1afc",
-        "image": "https://github.com/aayushsh2003/tailwindportfolio/blob/main/ALL_Things/Certificates/IIRS.png?raw=true"
-      },
-      {
-        "title": "Enhancing Soft Skills and Personality",
-        "issuingOrganization": {
-          "name": "Indian Institute of Technology, Kanpur",
-          "url": "https://nptel.ac.in/"
-        },
-        "description": "NPTEL course on soft skills and personality development, scored 76%.",
-        "issueDate": "2024-04",
-        "credentialID": "NPTEL24HS26S457801776",
-        "image": "https://github.com/aayushsh2003/tailwindportfolio/blob/main/ALL_Things/NPTEL/Enhancing%20Soft%20Skills%20and%20Personality.png?raw=true"
-      },
-      {
-        "title": "Developing Soft Skills and Personality",
-        "issuingOrganization": {
-          "name": "Indian Institute of Technology, Kanpur",
-          "url": "https://nptel.ac.in/"
-        },
-        "description": "NPTEL course on developing soft skills.",
-        "issueDate": "2023-09",
-        "credentialID": "NPTEL23HS116S45260102",
-        "image": "https://github.com/aayushsh2003/tailwindportfolio/blob/main/ALL_Things/NPTEL/Developing%20Soft%20Skills%20and%20Personality.png?raw=true"
-      },
-      {
-        "title": "Problem Solving Through Programming in C",
-        "issuingOrganization": {
-          "name": "Indian Institute of Technology, Kharagpur",
-          "url": "https://nptel.ac.in/"
-        },
-        "description": "NPTEL certification in C programming problem-solving.",
-        "issueDate": "2023-04",
-        "credentialID": "NPTEL23CS53543791284",
-        "image": "https://github.com/aayushsh2003/tailwindportfolio/blob/main/ALL_Things/NPTEL/Problem%20Solving%20Through%20Programming%20In%20C.jpg?raw=true"
-      },
-      {
-        "title": "The Joy of Computing Using Python",
-        "issuingOrganization": {
-          "name": "Indian Institute of Technology, Madras",
-          "url": "https://nptel.ac.in/"
-        },
-        "description": "NPTEL certification in Python programming.",
-        "issueDate": "2023-01",
-        "credentialID": "NPTEL23CS20533790773",
-        "image": "https://github.com/aayushsh2003/tailwindportfolio/blob/main/ALL_Things/NPTEL/The%20Joy%20of%20Computing%20using%20Python.jpg?raw=true"
       }
   ],
 
@@ -566,19 +389,6 @@ export const portfolioData = {
         "Led a team comprising Utkarsh Batham, Aman Singhal, Shivam Raj, and Pravesh Choudhary to present an innovative model.",
         "Developed and showcased the 'Unique Candidate Unique Question Paper' (UCUQ) Model, a computer-based assessment system."
       ]
-    },
-    {
-      "title": "Poornima Nexus Project (Non-Syllabus Project)",
-      "role": "Main Developer and Technical Lead",
-      "organization": {
-        "name": "Poornima College of Engineering",
-        "url": "https://www.poornima.org/"
-      },
-      "description": [
-        "Led the technical development and implementation of the Poornima Nexus Project, a pioneering non-syllabus initiative.",
-        "Designed and developed core functionalities using modern web technologies and frameworks.",
-        "Collaborated with faculty and students to ensure seamless integration and usability."
-      ]
     }
   ],
   "honorsAndAwards": [
@@ -595,34 +405,6 @@ export const portfolioData = {
       "date": "November 2023",
       "description": "Received my voter certificate as a responsible citizen, marking my first participation in state elections.",
       "image": "https://github.com/aayushsh2003/tailwindportfolio/blob/main/ALL_Things/Awards/Rajasthan%20Vidhan%20Sabha%20Election.jpeg?raw=true"
-    },
-    {
-      "title": "Ganga Quest 2021 Winner",
-      "issuer": "Ministry of Jal Shakti",
-      "date": "November 2021",
-      "description": "Honored to be among the top 25 winners of Ganga Quest 2021, recognizing my knowledge and commitment to river conservation.",
-      "image": "https://media.licdn.com/dms/image/v2/D4E22AQFZqSY4DmHR2A/feedshare-shrink_800/feedshare-shrink_800/0/1699882153450?e=1746057600&v=beta&t=-f9M0D8xNIKMkpjbWpljscNLl1gSj8QUuQbjjxYDCK8"
-    },
-    {
-      "title": "Certificate of Merit in Mathematics Standard",
-      "issuer": "Central Board of Secondary Education (CBSE)",
-      "date": "October 2021",
-      "description": "Awarded for outstanding performance and being in the top 0.1% of successful candidates in Mathematics Standard.",
-      "image": "https://github.com/aayushsh2003/tailwindportfolio/blob/main/ALL_Things/Awards/10thBoard.jpeg?raw=true"
-    },
-    {
-      "title": "PCRA Quiz Competition 2019 - Bronze Level",
-      "issuer": "Ministry of Petroleum and Natural Gas",
-      "date": "September 2019",
-      "description": "Achieved Bronze level in an individual quiz competition on oil and gas conservation.",
-      "image": "https://github.com/aayushsh2003/tailwindportfolio/blob/main/ALL_Things/Awards/PCRA_QUIZ.jpeg?raw=true"
-    },
-    {
-      "title": "Rajasthan State Certificate in Information Technology",
-      "issuer": "Vardhman Mahaveer Open University, Kota",
-      "date": "October 2018",
-      "description": "Successfully completed the Rajasthan State Certificate in Information Technology (RS-CIT) program.",
-      "image": "https://github.com/aayushsh2003/tailwindportfolio/blob/main/ALL_Things/Awards/Rs-Cit.jpeg?raw=true"
     }
   ],
   "languages": ["Hindi", "English"]
